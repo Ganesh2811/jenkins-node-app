38ee29e47b49caa1e1ef98697bb7632236efbf57
Ganesh2811 <gp11282001@gmail.com>
2026-01-13 19:28:17 +0530

    changes added

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 
 const express = require("express");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const routes = require("./controller/routes");
-const config = require("./config")
+// const cookieParser = require("cookie-parser");
+// const cors = require("cors");
+// const routes = require("./controller/routes");
+// const config = require("./config")
 
 const app = express();
-app.use(express.json());
-app.use(cookieParser());
+// app.use(express.json());
+// app.use(cookieParser());
 
-const port = config?.port;
+const port = "8000" || config?.port;
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+// app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
-const daoMiddleware = require("./middleware").daoMiddleware;
-app.use(daoMiddleware);
-app.use("/api", routes);
+// const daoMiddleware = require("./middleware").daoMiddleware;
+// app.use(daoMiddleware);
+// app.use("/api", routes);
+
+app.get("/api/sample", (req, res, next) => {
+    res.send({status: true, data: "Data gets successfully !!"})
+})
 
 app.listen(port, () => {
     console.log('The magic happens on port ' + port + ' Date' + Date());
